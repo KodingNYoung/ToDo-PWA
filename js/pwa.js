f3333d3bc7596638028b137df01dc4beefed56b0
Adebambo Abiodun Oluwasegun <abiodunadebambo44@gmail.com>
2020-06-22 18:52:16 +0100

    omitted the fu word

--- js/pwa.js
+++ js/pwa.js
@@ -1,6 +1,6 @@
 // check if the browser supports service workers
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("../serviceWorker.js/", {scope: "fu/ToDo-PWA/")
+    navigator.serviceWorker.register("../serviceWorker.js/", {scope: "/ToDo-PWA/")
         .then((regResponse) => console.log("Registered service worker", regResponse))
         .catch((errMessage) => console.log("couldn't load service worker", errMessage))
 }
