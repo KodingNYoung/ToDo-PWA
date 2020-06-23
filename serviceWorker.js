88cef5cfc5152bd7706e75238e5aa4514b631f22
Young <abiodunadebambo44@gmail.com>
2020-06-23 04:00:06 +0100

    testing

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,10 +1,6 @@
 // set a cache storage name
 const cacheName = 'static-assets';
 const assets = [
-    "/",
-    "index.html",
-    "js/script.js",
-    "css/style.css",
     "https://kit.fontawesome.com/1395a25f53.js",
     "https://kit-free.fontawesome.com"
 ]
