f980e8a7488023eab70f1015d39766d212ba3a6e
Young <abiodunadebambo44@gmail.com>
2020-06-23 01:18:12 +0100

    enabled offline mode w service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,29 @@
+// set a cache storage name
+const cacheName = 'static-assets'
+const assets = [
+    "/",
+    "/index.html",
+    "js/pwa.js",
+    "js/script.js",
+    "css/style.css",
+    "https://kit.fontawesome.com/1395a25f53.js",
+    "https://kit-free.fontawesome.com"
+]
+
+
 // checking for events
 // install
 self.addEventListener("install", (instEvt) => {
-    console.log("Service worker installed", instEvt);
+    // console.log("Service worker installed", instEvt);
+    // When the install event is taking place
+    // hold the event until the caching is done
+    instEvt.waitUntil(
+        // open a cache storage
+        caches.open(cacheName).then((cache) => {
+            // then add all assets
+            cache.addAll(assets)
+        })
+    )
 });
 
 // activate
@@ -11,5 +33,16 @@ self.addEventListener("activate", (actEvt) => {
 
 // fetch
 self.addEventListener("fetch", (evt) => {
-    console.log("Fetched", evt.request.url)
+    // console.log("Fetched", evt.request.url)
+    // When there is a fetch event
+    // first check with the cache storage
+    evt.respondWith(
+        caches.match(evt.request).then((cacheVal) => {
+            if (cacheVal){
+                return cacheVal;
+            }else{
+                return fetch(evt.request);
+            }
+        })
+    )
 })
